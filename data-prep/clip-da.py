31f8d12378f5b5bb824facb7d051d6f2d7046350
Jeff Allen <jeffallen89@gmail.com>
2023-04-24 12:52:03 -0400

    after clip, extract just poly from geom collection

--- data-prep/clip-da.py
+++ data-prep/clip-da.py
@@ -1,35 +1,39 @@
 import geopandas as gpd
+import pandas as pd
 from shapely.geometry import Polygon, MultiPolygon, GeometryCollection
 
-# da = gpd.read_file("gtha-da-2021.geojson")
+da = gpd.read_file("gtha-da-2021.geojson")
 
-# clip = gpd.read_file("data/green.geojson")
-# clip = clip[clip.geometry.apply(lambda x: isinstance(x, (Polygon, MultiPolygon)))]
+clip = gpd.read_file("data/green.geojson")
+clip = clip[clip.geometry.apply(lambda x: isinstance(x, (Polygon, MultiPolygon)))]
 
-# da = gpd.overlay(da, clip, how='difference', keep_geom_type = True)
+da = gpd.overlay(da, clip, how='difference', keep_geom_type = True)
 
-# del clip
+del clip
 
-# clip = gpd.read_file("data/water.geojson")
-# clip = clip[clip.geometry.apply(lambda x: isinstance(x, (Polygon, MultiPolygon)))]
+clip = gpd.read_file("data/water.geojson")
+clip = clip[clip.geometry.apply(lambda x: isinstance(x, (Polygon, MultiPolygon)))]
 
-# da = gpd.overlay(da, clip, how='difference', keep_geom_type = True)
+da = gpd.overlay(da, clip, how='difference', keep_geom_type = True)
 
-# del clip
+del clip
 
-# clip = gpd.read_file("data/built-non-res.geojson")
-# clip = clip[clip.geometry.apply(lambda x: isinstance(x, (Polygon, MultiPolygon)))]
+clip = gpd.read_file("data/built-non-res.geojson")
+clip = clip[clip.geometry.apply(lambda x: isinstance(x, (Polygon, MultiPolygon)))]
 
-# da = gpd.overlay(da, clip, how='difference', keep_geom_type = True)
+da = gpd.overlay(da, clip, how='difference', keep_geom_type = True)
 
 # da.to_file('gtha-da-2021-clipped.geojson', driver='GeoJSON')
 
-da = gpd.read_file("gtha-da-2021-clipped.geojson")
+# da = gpd.read_file("gtha-da-2021-clipped.geojson")
 
 dap = da[da.geometry.apply(lambda x: isinstance(x, (Polygon, MultiPolygon)))]
 
-print(dap)
-
 dag = da[da.geometry.apply(lambda x: isinstance(x, (GeometryCollection)))]
+dag = dag.explode()
+dag = dag[dag.geometry.apply(lambda x: isinstance(x, (Polygon, MultiPolygon)))]
+dag = dag.dissolve('DAUID', as_index = False)
 
-print(dag)
+da = pd.concat([dap, dag], ignore_index=True)
+
+da.to_file('gtha-da-2021-clipped.geojson', driver='GeoJSON')
